5bfa5ca28e73522d1710e40e986061c547e1eea7
Your Name <you@example.com>
2022-10-18 17:18:49 +0300

    semi

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:citycafe_app/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+
+runApp(MaterialApp(home: MyApp(),));
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Login_screen();
+  }
+}
